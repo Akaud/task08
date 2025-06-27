c095a9cd18f28cf82a086cff8d240965a9eed238
Akaud <akadlevchenko@gmail.com>
2025-06-27 09:26:47 +0300

    try_012

--- task08/versions.tf
+++ task08/versions.tf
@@ -38,4 +38,5 @@ provider "kubectl" {
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+  load_config_file = false
 }
